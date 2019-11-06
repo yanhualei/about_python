f92c40cdc743615ebd4d5c6795ea154b0dc96598
yanhualei <1208832367@qq.com>
2019-11-07 02:59:51 +0800

    master修改文件004

--- python_learning/python_projects/from_dev_004.py
+++ python_learning/python_projects/from_dev_004.py
@@ -1,4 +1,18 @@
-import random
+# 二分查找
+def half_search(target,my_list):
+    start = 0
+    end = len(my_list)-1
+    while start <= end:
+        mid = (start + end) // 2
+        if my_list[mid] == target:
+            return mid
+        if my_list[mid] <= target:
+            start = mid
+        if my_list[mid] > target:
+            end = mid
 
-li = [i for i in range(random.randint(1,100))]
-print(li)
+if __name__ == '__main__':
+    my_list = [1,2,3,4,5,6,7,8,9]
+    target =6
+    result = half_search(target,my_list)
+    print(result)
